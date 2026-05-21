a80989eb53c298328d436e873d362e515114a2e5
ASdev-coder <ant.skliar777@gmail.com>
2026-05-21 23:28:44 +0300

    refactor(router): оновити маршрути застосунку

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,18 +1,27 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { ChatRoom } from "./pages/ChatRoom";
+import { SearchingPage } from "./pages/SearchingPage";
+import { PrivacyPage } from "./pages/PrivacyPage";
+import { GuidelinesPage } from "./pages/GuidelinesPage";
 import { LanguageProvider } from "./context/LanguageProvider";
 import { ThemeProvider } from "./context/ThemeProvider";
+import { SocketProvider } from "./context/SocketContext";
 import "./styles/global.css";
 
 function App() {
   return (
     <LanguageProvider>
       <ThemeProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/chat" element={<ChatRoom />} />
-        </Routes>
+        <SocketProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/searching" element={<SearchingPage />} />
+            <Route path="/chat" element={<ChatRoom />} />
+            <Route path="/privacy" element={<PrivacyPage />} />
+            <Route path="/guidelines" element={<GuidelinesPage />} />
+          </Routes>
+        </SocketProvider>
       </ThemeProvider>
     </LanguageProvider>
   );
